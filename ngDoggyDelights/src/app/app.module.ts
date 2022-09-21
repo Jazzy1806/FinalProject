fa352993e5d377730b677f1c17e4f388a76b5a24
Jazzy1806 <92125149+Jazzy1806@users.noreply.github.com>
2022-09-21 14:57:41 -0600

    nav and logout compts

--- ngDoggyDelights/src/app/app.module.ts
+++ ngDoggyDelights/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+//    HomeComponent,
+//    LoginComponent,
+    LogoutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [NgbActiveModal, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
