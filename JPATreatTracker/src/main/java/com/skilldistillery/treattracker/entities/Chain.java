68871d64af629210c72fff2abdb226efea317c8f
thaodev <71045132+thaodev@users.noreply.github.com>
2022-09-21 17:39:28 -0600

    Add Address, store, and chain entities

---
+++ JPATreatTracker/src/main/java/com/skilldistillery/treattracker/entities/Chain.java
@@ -0,0 +1,73 @@
+package com.skilldistillery.treattracker.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Chain {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	private String description;
+	
+	@Column(name="website_url")
+	private String websiteUrl;
+	
+	@Column(name="logo_url")
+	private String logoUrl;
+
+	
+	public Chain() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getWebsiteUrl() {
+		return websiteUrl;
+	}
+
+	public void setWebsiteUrl(String websiteUrl) {
+		this.websiteUrl = websiteUrl;
+	}
+
+	public String getLogoUrl() {
+		return logoUrl;
+	}
+
+	public void setLogoUrl(String logoUrl) {
+		this.logoUrl = logoUrl;
+	}
+	
+	
+	
+
+}
