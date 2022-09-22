712c6adbba29514c3ac728c4b0ea2679668c3b84
thaodev <71045132+thaodev@users.noreply.github.com>
2022-09-21 18:46:25 -0600

    Add Store comment

---
+++ JPATreatTracker/src/main/java/com/skilldistillery/treattracker/entities/Inventory.java
@@ -0,0 +1,71 @@
+package com.skilldistillery.treattracker.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Inventory {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@ManyToOne
+	@JoinColumn(name="store_id")
+	private Store store;
+	
+	private double price;
+	
+	private int quantity;
+	
+
+	public Inventory() {
+	
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Store getStore() {
+		return store;
+	}
+
+	public void setStore(Store store) {
+		this.store = store;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Inventory [id=" + id + ", store=" + store + ", price=" + price + ", quantity=" + quantity + "]";
+	}
+	
+	
+	
+
+}
