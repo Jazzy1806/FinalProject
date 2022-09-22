e2e5cafb77105cd1df0b50cac57ead9d55263c2a
Jazzy1806 <92125149+Jazzy1806@users.noreply.github.com>
2022-09-22 09:42:34 -0600

    model buildout

--- ngDoggyDelights/src/app/models/address.ts
+++ ngDoggyDelights/src/app/models/address.ts
@@ -1,2 +1,17 @@
 export class Address {
+  id: number;
+  address: string | null;
+  city: string | null;
+  state: string | null;
+  postalCode: string | null;
+  phone: string | null;
+
+  constructor(  id: number, address: string, city: string, state: string, postalCode: string, phone: string) {
+    this.id = id;
+    this.address = address;
+    this.city = city;
+    this.state = state;
+    this.postalCode = postalCode;
+    this.phone = phone;
+  }
 }
--- ngDoggyDelights/src/app/models/product-comment.ts
+++ ngDoggyDelights/src/app/models/product-comment.ts
@@ -1,2 +1,15 @@
 export class ProductComment {
+  id: number;
+  title: string | null;
+  description: string | null;
+  rating: number | null;
+  dateCreated: Date | null;
+
+  constructor(id: number, title: string, description: string, rating: number, dateCreated: Date;) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.rating = rating;
+    this.dateCreated = dateCreated;
+  }
 }
