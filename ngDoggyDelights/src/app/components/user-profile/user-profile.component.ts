8fa44da5a0667a5b1c7cdad3f0dc31a0df84c6c7
thaodev <71045132+thaodev@users.noreply.github.com>
2022-09-25 12:00:54 -0600

    uncomment the user-profile component

--- ngDoggyDelights/src/app/components/user-profile/user-profile.component.ts
+++ ngDoggyDelights/src/app/components/user-profile/user-profile.component.ts
@@ -19,26 +19,26 @@ export class UserProfileComponent implements OnInit {
   constructor(private auth: AuthService,private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    // let idStr = this.route.snapshot.paramMap.get('id');
-    // if (idStr) {
-    //   let userId = Number.parseInt(idStr);
-    //   if (!isNaN(userId)) {
-    //     this.auth.getLoggedInUser().subscribe({
-    //       next: (userData) => {
-    //         this.loggedInUser = userData;
-    //       },
-    //       error: (err) => {
-    //         console.error("Error retrieving todo:");
-    //         console.error(err);
-    //         //if todo id doesn't exist, will direct user to notFound page
-    //         this.router.navigateByUrl('noSuchTodo');
-    //       }
-    //     })
-    //   // }
-    //   else {
-    //     console.error("No such user: " + idStr);
-    //   }
-    // }
+    let idStr = this.route.snapshot.paramMap.get('id');
+    if (idStr) {
+      let userId = Number.parseInt(idStr);
+      if (!isNaN(userId)) {
+        this.auth.getLoggedInUser().subscribe({
+          next: (userData) => {
+            this.loggedInUser = userData;
+          },
+          error: (err) => {
+            console.error("Error retrieving todo:");
+            console.error(err);
+            //if todo id doesn't exist, will direct user to notFound page
+            this.router.navigateByUrl('noSuchTodo');
+          }
+        })
+      // }
+      else {
+        console.error("No such user: " + idStr);
+      }
+    }
    }
 
 }
